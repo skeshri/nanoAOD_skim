8488833f440d3b46343fcc2f1657d242be232017
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-03-31 16:07:28 +0200

    Updated HZZ

--- HZZAnalysisModule.py
+++ HZZAnalysisModule.py
@@ -1,10 +1,10 @@
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
-class wvAnalysisProducer(Module):
+class HZZAnalysisProducer(Module):
     def __init__(self):
         pass
     def beginJob(self):
@@ -13,7 +13,7 @@ class wvAnalysisProducer(Module):
         pass
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        #self.out.branch("EventMass",  "F");
+        self.out.branch("nmergedjet",  "I");
         """process event, return True (go to next module) or False (fail, go to next event)"""
         #pass
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
@@ -21,17 +21,17 @@ class wvAnalysisProducer(Module):
     def analyze(self, event):
         """nanoAOD skimming is done considering the final events selection
         for the vv semileptonic final state.
-        
-        For the vv semileptonic final state we should be either one or two 
-        tight leptons, either one Fat jet and two small radius jet or four 
+
+        For the vv semileptonic final state we should be either one or two
+        tight leptons, either one Fat jet and two small radius jet or four
         small radius jets.
-        
+
         Arguments:
             event {instance of event} -- instance of event
-        
+
         Returns:
             boolean -- if the event passes skimming then it returns true and
-                       go to the next module else returns false and go to 
+                       go to the next module else returns false and go to
                        the next event.
         """
         electrons = Collection(event, "Electron")
@@ -43,26 +43,33 @@ class wvAnalysisProducer(Module):
         eventMuons = 0
         eventJets = 0
         eventFatJets = 0
-        
+        eventLeptons = 0
+
         for lep in muons :
-            if lep.tightId and lep.pt > 10 :
+            if lep.tightId and lep.pt > 5 :
                 eventMuons += 1
         for lep in electrons :
-            if lep.cutBased >= 2 and lep.pt > 10 :
+            if lep.cutBased >3 and lep.pt > 7 :
                 eventElectrons += 1
+
+        eventLeptons = eventElectrons+eventMuons
+
         for jet in jets :
             if jet.pt > 20:
                eventJets += 1
         for fatjet in fatJets :
-            if fatjet.pt > 20:
+            if fatjet.pt > 200:
                eventFatJets += 1
-        
-        if not ( ((eventElectrons >= 1 or eventMuons >=1)) and ( (eventJets >= 2 and eventFatJets >= 1) or (eventJets >= 4 and eventFatJets == 0) )  ):
+
+
+        if not ( eventLeptons>=2 and eventFatJets>=1 ):
             keepIt = False
-        
+        else:
+            keepIt = True
+            self.out.fillBranch("nmergedjet",fatJets.nFatJet)
+
         return keepIt
 
 
 # define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
-wvAnalysisModule = lambda : wvAnalysisProducer() #(jetSelection= lambda j : j.pt > 30) 
- 
+HZZAnalysisModule = lambda : HZZAnalysisProducer() #(jetSelection= lambda j : j.pt > 30)
--- post_proc.py
+++ post_proc.py
@@ -2,15 +2,16 @@
 import os,sys
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
-from wvAnalysisModule import *
+from HZZAnalysisModule import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import *
 from JetSFMaker import *
 
-testfile = "root://cms-xrd-global.cern.ch//store/data/Run2018A/SingleMuon/NANOAOD/Nano25Oct2019-v1/20000/D03C6AE0-73AD-A940-B8CA-779A621D4853.root"
+# testfile = "root://cms-xrd-global.cern.ch//store/data/Run2018A/SingleMuon/NANOAOD/Nano25Oct2019-v1/20000/D03C6AE0-73AD-A940-B8CA-779A621D4853.root"
+testfile = "root://cms-xrd-global.cern.ch//store/mc/RunIISummer19UL17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_mc2017_realistic_v6-v2/270000/07E11789-6AC3-9943-A770-53E366C5A7A7.root"
 
 
-entriesToRun = 0  # 0 if need to run over all entries else put number of entries to run
+entriesToRun = 100  # 0 if need to run over all entries else put number of entries to run
 isMCTrueFalse = True
 Year = 2016
 # Keep DownloadFileToLocalThenRun=True this should reduce the file read error from eos.
@@ -31,13 +32,14 @@ if testfile.find("SingleMuon") != -1 or testfile.find("EGamma") != -1 or testfil
   print "===> JSON File: ",jsonFileName
   jetmetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=Year, jesUncert="Merged", jetType = "AK4PFchs")
   fatJetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=Year, jesUncert="Merged", jetType = "AK8PFPuppi")
-  p=PostProcessor(".",[testfile],None,None,[wvAnalysisModule(),jetmetCorrector(),fatJetCorrector()],provenance=False,fwkJobReport=False,jsonInput=jsonFileName,maxEntries=entriesToRun,haddFileName="nano.root",prefetch=DownloadFileToLocalThenRun)
+  p=PostProcessor(".",[testfile],None,None,[HZZAnalysisModule(),jetmetCorrector(),fatJetCorrector()],provenance=False,fwkJobReport=False,jsonInput=jsonFileName,maxEntries=entriesToRun,haddFileName="nano.root",prefetch=DownloadFileToLocalThenRun)
 else:
   print "==> Processing a MC file..."
   isMCTrueFalse=True
   if testfile.find("RunIIAutumn18NanoAODv") != -1: year = 2018
   if testfile.find("VVjj_2018v") != -1: year = 2018
   if testfile.find("RunIIFall17NanoAODv") != -1: year = 2017
+  if testfile.find("RunIISummer19UL17NanoAOD") != -1: year = 2017
   if testfile.find("VVjj_2017v") != -1: year = 2017
   if testfile.find("RunIISummer16NanoAODv") != -1: year = 2016
   if testfile.find("VVjj_2016v") != -1: year = 2016
@@ -54,7 +56,7 @@ else:
     sfFileName="DeepCSV_102XSF_V2.csv"
   btagSF = lambda: btagSFProducer(era,algo="deepcsv",selectedWPs=['L','M','T','shape_corr'],sfFileName=sfFileName)
   puidSF = lambda: JetSFMaker("%s" % year)
-  p=PostProcessor(".",[testfile],"",None,[wvAnalysisModule(),jetmetCorrector(),fatJetCorrector(),btagSF(),puidSF()],provenance=True,fwkJobReport=False,maxEntries=entriesToRun,haddFileName="nano.root",prefetch=DownloadFileToLocalThenRun)
+  p=PostProcessor(".",[testfile],"",None,[HZZAnalysisModule(),jetmetCorrector(),fatJetCorrector(),btagSF(),puidSF()],provenance=True,fwkJobReport=False,maxEntries=entriesToRun,haddFileName="nano.root",prefetch=DownloadFileToLocalThenRun)
 
 p.run()
 print "DONE"
