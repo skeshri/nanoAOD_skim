937b46b82c5ae592f0e9b7087e7162065ef3d90e
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-06-13 22:48:55 +0200

    Updated json file

--- post_proc.py
+++ post_proc.py
@@ -20,35 +20,38 @@ if testfile.find("SingleMuon") != -1 or testfile.find("EGamma") != -1 or testfil
   isMCTrueFalse=False
   if testfile.find("Run2016") != -1:
     Year=2016
-    jsonFileName="Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt"
+    jsonFileName="Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"
   if testfile.find("Run2017") != -1:
     Year=2017
-    jsonFileName="Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"
+    jsonFileName="Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt"
   if testfile.find("Run2018") != -1:
     Year=2018
-    jsonFileName="Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt"
+    jsonFileName="Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"
   jsonFileName = "data/jsonFiles/"+jsonFileName
   print "\n===> Running over ",Year," data...\n"
   print "===> JSON File: ",jsonFileName
   # jetmetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=Year, jesUncert="Merged", jetType = "AK4PFchs")
   fatJetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=Year, jesUncert="Merged", jetType = "AK8PFPuppi")
-  p=PostProcessor(".",[testfile],None,None,[HZZAnalysisModule(),fatJetCorrector()],provenance=False,fwkJobReport=False,jsonInput=jsonFileName,maxEntries=entriesToRun,haddFileName="nano.root",prefetch=DownloadFileToLocalThenRun)
+  # p=PostProcessor(".",[testfile],None,None,[HZZAnalysisModule(),fatJetCorrector()],provenance=False,fwkJobReport=False,jsonInput=jsonFileName,maxEntries=entriesToRun,prefetch=DownloadFileToLocalThenRun)
+  p=PostProcessor(".",[testfile],None,None,[HZZAnalysisModule()],provenance=False,fwkJobReport=False,jsonInput=jsonFileName,maxEntries=entriesToRun,prefetch=DownloadFileToLocalThenRun)
 else:
   print "==> Processing a MC file..."
   isMCTrueFalse=True
   if testfile.find("RunIIAutumn18NanoAODv") != -1: year = 2018
   if testfile.find("VVjj_2018v") != -1: year = 2018
   if testfile.find("RunIIFall17NanoAODv") != -1: year = 2017
+  # if testfile.find("RunIISummer19UL17NanoAOD") != -1: year = 'UL2017'
   if testfile.find("RunIISummer19UL17NanoAOD") != -1: year = 2017
   if testfile.find("VVjj_2017v") != -1: year = 2017
   if testfile.find("RunIISummer16NanoAODv") != -1: year = 2016
   if testfile.find("VVjj_2016v") != -1: year = 2016
   # jetmetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=year, jesUncert="Merged", jetType = "AK4PFchs")
-  fatJetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=year, jesUncert="Merged", jetType = "AK8PFPuppi")
+  # fatJetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=year, jesUncert="Total", jetType = "AK8PFPuppi")
+  # fatJetCorrector = createJMECorrector(isMC=isMCTrueFalse, dataYear=year, jesUncert="Merged", jetType = "AK4PFchs")
   if year == 2016:
     era="Legacy2016"
     sfFileName="DeepCSV_2016LegacySF_V1.csv"
-  if year == 2017:
+  if year == 2017 or year == 'UL2017':
     era="2017"
     sfFileName="DeepCSV_94XSF_V5_B_F.csv"
   if year == 2018:
@@ -56,7 +59,9 @@ else:
     sfFileName="DeepCSV_102XSF_V2.csv"
   btagSF = lambda: btagSFProducer(era,algo="deepcsv",selectedWPs=['L','M','T','shape_corr'],sfFileName=sfFileName)
   puidSF = lambda: JetSFMaker("%s" % year)
-  p=PostProcessor(".",[testfile],"",None,[HZZAnalysisModule(),fatJetCorrector(),btagSF(),puidSF()],provenance=True,fwkJobReport=False,maxEntries=entriesToRun,haddFileName="nano.root",prefetch=DownloadFileToLocalThenRun)
+  #p=PostProcessor(".",[testfile],"",None,[HZZAnalysisModule(),fatJetCorrector(),btagSF(),puidSF()],provenance=True,fwkJobReport=False,maxEntries=entriesToRun,prefetch=DownloadFileToLocalThenRun)
+  # p=PostProcessor(".",[testfile],"",None,[HZZAnalysisModule(),fatJetCorrector()],provenance=True,fwkJobReport=False,maxEntries=entriesToRun,prefetch=DownloadFileToLocalThenRun)
+  p=PostProcessor(".",[testfile],"",None,[HZZAnalysisModule(),btagSF(),puidSF()],provenance=True,fwkJobReport=False,maxEntries=entriesToRun,prefetch=DownloadFileToLocalThenRun)
 
 p.run()
 print "DONE"
