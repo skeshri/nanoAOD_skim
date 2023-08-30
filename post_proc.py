f0acab1848166cf578841ab623b86f15aa204738
Anusree <anusree.vijay@cern.ch>
2023-08-30 10:41:11 +0200

    added new root file

--- post_proc.py
+++ post_proc.py
@@ -11,18 +11,18 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import 
 # testfile = "root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18NanoAODv9/GluGluHToZZTo4L_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/120000/3ED05633-EBB7-4A44-8F9D-CD956490BCFD.root"
 
 # 2l2q ggH Sample 1000 GeV
-testfile = "root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18NanoAODv9/GluGluHToZZTo2L2Q_M1000_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/70000/F920B631-39E7-EE4C-9D8D-B242A2103115.root"
-
+#testfile = "root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18NanoAODv9/GluGluHToZZTo2L2Q_M1000_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/70000/F920B631-39E7-EE4C-9D8D-B242A2103115.root"
+testfile = "root://cms-xrd-global.cern.ch//store/data/Run2018A/DoubleMuon/MINIAOD/UL2018_MiniAODv2_GT36-v1/2430000/00267D27-22E1-C143-A50C-5FC81AE13C86.root"
 # 2l2nu ggH Sample 1000 GeV
 # testfile = "root://cms-xrd-global.cern.ch/"
 
-entriesToRun = 0 # 0 if need to run over all entries else put number of entries to run
+entriesToRun = 100 # 0 if need to run over all entries else put number of entries to run
 isMCTrueFalse = True
 # Keep DownloadFileToLocalThenRun=True this should reduce the file read error from eos.
 DownloadFileToLocalThenRun=True
-if testfile.find("UL18") != -1:
+if testfile.find("UL2018") != -1:
     year = 2018
-if testfile.find("UL17") != -1:
+if testfile.find("UL2017") != -1:
     year = 2017
 H4LCppModule = lambda: HZZAnalysisCppProducer(year)
 #p=PostProcessor(".",[testfile],"",None,[H4LCppModule()],provenance=True,fwkJobReport=False,haddFileName="nano_M125.root",maxEntries=entriesToRun,prefetch=DownloadFileToLocalThenRun,outputbranchsel="keep_and_drop.txt")
