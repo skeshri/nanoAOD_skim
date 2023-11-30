c9b0fccc4a976af660119549442329728fb3b3d4
Ramkrishna Sharma <ram1123@users.noreply.github.com>
2023-11-30 23:37:45 +0100

    Create mymodule.py

---
+++ mymodule.py
@@ -0,0 +1,26 @@
+# mymodule.py
+
+def add(a, b):
+    """
+    Return the sum of two numbers.
+    
+    Parameters:
+    a (int, float): The first number.
+    b (int, float): The second number.
+
+    Returns:
+    int, float: The sum of a and b.
+    """
+    return a + b
+
+def greet(name):
+    """
+    Return a greeting string for the given name.
+    
+    Parameters:
+    name (str): The name to greet.
+
+    Returns:
+    str: A greeting message.
+    """
+    return f"Hello, {name}!"
