63560a47b3d3bcf5a39f48c5be8ecf1d6b15daf8
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2024-04-24 17:21:41 +0200

    The code removeDuplicate.C is not working, yet.

--- scripts/removeDuplicates.C
+++ scripts/removeDuplicates.C
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <TString.h>
 #include <TFile.h>
 #include <TTree.h>
+#include <TChain.h>
 
 void removeDuplicates()
 {
-
-    TString prefix = "Muon";
     TString outputFile = "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/Data_noDuplicates.root";
-
-        TString infiles[] = {"/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/DoubleMuon.root",
-                             "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/SingleMuon.root",
-                             "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/MuonEG.root",
-                             "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/Muon.root",
-                             "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/EGamma.root"};
+    TString infiles[] = {
+        "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/DoubleMuon.root",
+        "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/SingleMuon.root",
+        "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/MuonEG.root",
+        "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/Muon.root",
+        "/eos/user/r/rasharma/nanoAOD_ntuples/SkimNanoAOD_2022_ZXCRv2/EGamma.root"
+        };
 
     TChain *oldtree = new TChain("Events");
     for (TString infile : infiles)
@@ -22,23 +22,18 @@ void removeDuplicates()
         oldtree->Add(infile);
     }
 
-    // Run from Runs tree
-
     Long64_t nentries = oldtree->GetEntries();
     std::cout << nentries << " total entries." << std::endl;
+
     ULong64_t Run, LumiSect, Event;
-    bool passedZ4lSelection;
     oldtree->SetBranchAddress("run", &Run);
     oldtree->SetBranchAddress("luminosityBlock", &LumiSect);
     oldtree->SetBranchAddress("event", &Event);
 
-    // Create a new file + a clone of old tree in new file
-    // TFile *newfile = new TFile(
-    //     prefix + "_noDuplicates.root", "recreate");
     TFile *newfile = new TFile(outputFile, "recreate");
     TTree *newtree = oldtree->CloneTree(0);
 
-    std::set<TString> runlumieventSet;
+    std::unordered_set<std::string> runlumieventSet; // Changed to unordered_set for performance
     int nremoved = 0;
     for (Long64_t i = 0; i < nentries; i++)
     {
@@ -51,21 +46,18 @@ void removeDuplicates()
         TString s_Event = std::to_string(Event);
         TString runlumievent = s_Run + ":" + s_Lumi + ":" + s_Event;
 
-        if (runlumieventSet.find(runlumievent) == runlumieventSet.end())
+        if (runlumieventSet.insert(runlumievent).second)
         {
-            runlumieventSet.insert(runlumievent);
             newtree->Fill();
         }
         else
         {
             nremoved++;
         }
-        // if (passedZ4lSelection) newtree->Fill();
     }
 
-    std::cout << nremoved << " duplicates." << std::endl;
+    std::cout << nremoved << " duplicates removed." << std::endl;
     newtree->Print();
     newtree->AutoSave();
-    // delete oldfile;
     delete newfile;
 }
